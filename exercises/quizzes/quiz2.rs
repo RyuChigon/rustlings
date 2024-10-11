396092fbff4bed785500ba84a74d506bb9ae242f
RyuChigon <fbclrhs10@gmail.com>
2024-10-12 00:43:02 +0900

    enum, string, modules, hashmap, options

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,27 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut vec = Vec::<String>::new();
+
+        for (mut s, command) in input {
+            let a = match command {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_owned(),
+                Command::Append(size) => {
+                    for _ in 0..size {
+                        s += "bar";
+                    }
+
+                    s
+                }, 
+            };
+
+            vec.push(a);
+        }
+
+        vec
+    }
 }
 
 fn main() {
@@ -37,7 +57,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
